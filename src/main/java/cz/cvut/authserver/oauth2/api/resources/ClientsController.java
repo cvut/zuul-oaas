0252c5010338277b5e392148aa047195b9892158
Jakub Jirutka <jakub@jirutka.cz>
2012-10-19 21:17:02 +0200

    Add unfinished ClientsController

---
+++ src/main/java/cz/cvut/authserver/oauth2/api/resources/ClientsController.java
@@ -0,0 +1,117 @@
+package cz.cvut.authserver.oauth2.api.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.BaseClientDetails;
+import org.springframework.security.oauth2.provider.ClientAlreadyExistsException;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationService;
+import org.springframework.security.oauth2.provider.NoSuchClientException;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ *
+ * @author Tomáš Maňo <tomasmano@gmail.com>
+ * @author Jakub Jirutka <jakub@jirutka.cz>
+ */
+@Controller
+@RequestMapping(value="/v1/clients")
+public class ClientsController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ClientsController.class);
+
+    private ClientDetailsService clientDetailsService;
+    private ClientRegistrationService clientRegistrationService;
+
+
+
+    @ResponseBody
+    @RequestMapping(value="{clientId}", method=GET)
+    public ClientDetails getClientDetails(@PathVariable String clientId) throws Exception {
+        return clientDetailsService.loadClientByClientId(clientId);
+    }
+
+    @ResponseStatus(CREATED)
+    @RequestMapping(method=POST)
+    public void createClientDetails(@RequestBody BaseClientDetails client) throws Exception {
+        // TODO it MUST valide given data before insert!
+
+        clientRegistrationService.addClientDetails(client);
+
+        // TODO should send redirect to URI of the created client (i.e. api/clients/{clientId}/)
+    }
+
+    @ResponseStatus(NO_CONTENT)
+    @RequestMapping(value="{clientId}", method=PUT)
+    public void updateClientDetails(@RequestBody BaseClientDetails client,
+                                    @PathVariable String clientId) throws Exception {
+
+        Assert.state(clientId.equals(client.getClientId()), String.format(
+                "The client_id %s does not match the URL %s", client.getClientId(), clientId));
+
+        ClientDetails details = client;
+        try {
+            ClientDetails existing = getClientDetails(clientId);
+            // TODO it should sync given client with existing one
+        } catch (Exception ex) {
+            LOG.warn("Couldn't fetch client details for client_id: " + clientId, ex);
+        }
+        // TODO it MUST valide given data before update!
+        clientRegistrationService.updateClientDetails(details);
+    }
+
+    @ResponseStatus(NO_CONTENT)
+    @RequestMapping(value="{clientId}", method=DELETE)
+    public void removeClientDetails(@PathVariable String clientId) throws Exception {
+        // TODO check?
+        clientRegistrationService.removeClientDetails(clientId);
+    }
+
+    @ResponseStatus(NO_CONTENT)
+    @RequestMapping(value="{clientId}/secret", method=PUT)
+    public void updateClientSecret(@PathVariable String clientId, @RequestBody /* FIXME -> */ Object secretChangeRequest) throws Exception {
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        // TODO it MUST check given old password against what we load
+
+        // TODO -------------------------------------------------v
+        //clientRegistrationService.updateClientSecret(clientId, secretChangeRequest.getNewSecret());
+    }
+
+
+    @ExceptionHandler(NoSuchClientException.class)
+    public ResponseEntity<Void> handleNoSuchClient(NoSuchClientException ex) {
+            return new ResponseEntity<>(NOT_FOUND);
+    }
+
+    @ExceptionHandler(ClientAlreadyExistsException.class)
+    public ResponseEntity<Void> handleClientAlreadyExists(ClientAlreadyExistsException ex) {
+            return new ResponseEntity<>(CONFLICT);
+    }
+
+
+    ////////  Getters / Setters  ////////
+
+    public void setClientDetailsService(ClientDetailsService clientDetailsService) {
+        this.clientDetailsService = clientDetailsService;
+    }
+
+    public void setClientRegistrationService(ClientRegistrationService clientRegistrationService) {
+        this.clientRegistrationService = clientRegistrationService;
+    }
+    
+}
+
---
+++ src/test/java/cz/cvut/authserver/oauth2/api/resources/ClientsControllerTest.java
@@ -0,0 +1,159 @@
+package cz.cvut.authserver.oauth2.api.resources;
+
+import cz.cvut.authserver.oauth2.Factories;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationService;
+import org.springframework.security.oauth2.provider.NoSuchClientException;
+import org.springframework.test.web.server.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.server.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ *
+ * @author Jakub Jirutka <jakub@jirutka.cz>
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ClientsControllerTest {
+
+    private static final String BASE_URI = "/v1/clients/";
+    private static final String MIME_TYPE_JSON = "application/json;charset=UTF-8";
+
+    // JSON attributes
+    public static final String
+            CLIENT_ID = "client_id",
+            CLIENT_SECRET = "client_secret",
+            RESOURCE_IDS = "resource_ids",
+            GRANT_TYPES = "authorized_grant_types",
+            REDIRECT_URI = "redirect_uri",
+            ACCESS_TOKEN_VALIDITY = "access_token_validity",
+            REFRESH_TOKEN_VALIDITY = "refresh_token_validity";
+
+    private @Mock ClientRegistrationService clientRegistrationService;
+    private @Mock ClientDetailsService clientDetailsService;
+    private @InjectMocks ClientsController clientsController;
+
+    private MockMvc mock;
+
+
+    @Before
+    public void buildMocks() {
+        mock = standaloneSetup(clientsController).build();
+    }
+
+
+    @Test
+    public void get_client_details_for_non_existing_client_id() throws Exception {
+        doThrow(NoSuchClientException.class)
+                .when(clientDetailsService).loadClientByClientId("666");
+
+        mock.perform(get(BASE_URI + 666).accept(APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void get_client_details() throws Exception {
+        ClientDetails expected = Factories.createRandomClientDetails("42");
+        doReturn(expected).when(clientDetailsService).loadClientByClientId("42");
+
+        mock.perform(get(BASE_URI + 42).accept(APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().mimeType(MIME_TYPE_JSON))
+            .andExpect(jsonPath(CLIENT_ID, equalTo("42")))
+            .andExpect(jsonPath(CLIENT_SECRET, equalTo(expected.getClientSecret())))
+            .andExpect(jsonPath(RESOURCE_IDS, hasItems(expected.getResourceIds().toArray())))
+            .andExpect(jsonPath(GRANT_TYPES, hasItems(expected.getAuthorizedGrantTypes().toArray())))
+            .andExpect(jsonPath(REDIRECT_URI, hasItems(expected.getRegisteredRedirectUri().toArray())))
+            .andExpect(jsonPath(ACCESS_TOKEN_VALIDITY, equalTo(expected.getAccessTokenValiditySeconds())))
+            .andExpect(jsonPath(REFRESH_TOKEN_VALIDITY, equalTo(expected.getRefreshTokenValiditySeconds())));
+    }
+
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void create_client_details_with_invalid_data() throws Exception {
+        // TODO
+    }
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void create_client_details() throws Exception {
+        // TODO
+    }
+
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_details_for_invalid_data() throws Exception {
+        // TODO
+    }
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_details_for_client_id_does_not_match_url() throws Exception {
+        // TODO
+    }
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_details() throws Exception {
+        // TODO
+    }
+
+
+    @Test
+    public void remove_client_details_for_unkown_client_id() throws Exception {
+        doThrow(NoSuchClientException.class)
+                .when(clientRegistrationService).removeClientDetails("666");
+
+        mock.perform(delete(BASE_URI + 666))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void remove_client_details() throws Exception {
+        mock.perform(delete(BASE_URI + 123))
+            .andExpect(status().isNoContent());
+
+        verify(clientRegistrationService).removeClientDetails("123");
+    }
+
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_secret_for_invalid_data() throws Exception {
+        // TODO
+    }
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_secret_for_non_existing_client_id() throws Exception {
+        // TODO
+    }
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_secret_for_invalid_old_password() throws Exception {
+        // TODO
+    }
+
+    @Test
+    @Ignore("Not implemented yet")
+    public void update_client_secret() throws Exception {
+        // TODO
+    }
+
+}
