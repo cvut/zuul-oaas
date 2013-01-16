c1c2f011548f885a2bc56d5e95ae4f46a0697d91
Tomas Mano <tomasmano@gmail.com>
2013-01-16 17:16:30 +0100

    Enabling Security for Resource API

--- src/main/java/cz/cvut/authserver/oauth2/models/resource/Resource.java
+++ src/main/java/cz/cvut/authserver/oauth2/models/resource/Resource.java
@@ -34,7 +34,7 @@ public class Resource {
     @JsonProperty("name")
     private String name;
     
-    @NotNull(message="{required.version}") @Size(max=256)
+    @NotNull() @Size(max=256)
     @JsonProperty("version")
     private String version;
     
