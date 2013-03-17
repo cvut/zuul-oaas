a81877d22083e8fd02566b9ae6fba1084bc72cd9
Jakub Jirutka <jakub@jirutka.cz>
2013-03-17 01:39:16 +0100

    Localize templates

--- src/main/java/cz/cvut/authserver/oauth2/controllers/AccessConfirmationController.java
+++ src/main/java/cz/cvut/authserver/oauth2/controllers/AccessConfirmationController.java
@@ -1,27 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.cvut.authserver.oauth2.controllers;
-
-import java.util.Map;
 
 import cz.cvut.authserver.oauth2.dao.ClientDAO;
 import cz.cvut.authserver.oauth2.models.Client;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.AuthorizationRequest;
-import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 /**
  * Controller for retrieving the model for and displaying the confirmation page for access to a protected resource.
- * 
- * @author Ryan Heaton
+ *
+ * @author Tomas Mano <tomasmano@gmail.com>
+ * @author Jakub Jirutka <jakub@jirutka.cz>
  */
 @Controller
 @SessionAttributes("authorizationRequest")
@@ -32,17 +27,16 @@ public class AccessConfirmationController {
 	@RequestMapping("/oauth/confirm_access")
 	public ModelAndView getAccessConfirmation(Map<String, Object> model) throws OAuth2Exception {
 		AuthorizationRequest clientAuth = (AuthorizationRequest) model.remove("authorizationRequest");
+
 		Client client = clientDAO.findOne(clientAuth.getClientId());
 		model.put("auth_request", clientAuth);
 		model.put("client", client);
-		return new ModelAndView("access_confirmation", model);
+
+		return new ModelAndView("confirm_access", model);
 	}
 
 	@RequestMapping("/oauth/error")
 	public String handleError(Map<String,Object> model) {
-		// We can add more stuff to the model here for JSP rendering.  If the client was a machine then
-		// the JSON will already have been rendered.
-		model.put("message", "Nastala chyba v protokole OAuth 2.0.");
 		return "oauth_error";
 	}
 
