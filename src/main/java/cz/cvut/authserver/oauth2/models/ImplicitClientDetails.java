bbb5a9dad0e60439415196e0d2e705142dc5885d
Tomas Mano <tomasmano@gmail.com>
2013-04-09 19:49:25 +0200

    Add missing getters and setters

--- src/main/java/cz/cvut/authserver/oauth2/models/ImplicitClientDetails.java
+++ src/main/java/cz/cvut/authserver/oauth2/models/ImplicitClientDetails.java
@@ -17,5 +17,13 @@ public class ImplicitClientDetails {
     public ImplicitClientDetails(String type) {
         this.type = type;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
     
 }
