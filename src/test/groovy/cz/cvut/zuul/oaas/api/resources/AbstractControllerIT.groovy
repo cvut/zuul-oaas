ae79acdba09952a374a04a840903f9ddaed25b21
Jakub Jirutka <jakub@jirutka.cz>
2013-07-23 12:47:23 +0200

    Reimplement ClientsControllerIT with Spock

---
+++ src/test/groovy/cz/cvut/zuul/oaas/api/resources/AbstractControllerIT.groovy
@@ -0,0 +1,62 @@
+package cz.cvut.zuul.oaas.api.resources
+
+import cz.cvut.zuul.oaas.test.AdvicedStandaloneMockMvcBuilder
+import groovy.json.JsonSlurper
+import org.springframework.mock.web.MockHttpServletResponse
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author Jakub Jirutka <jakub@jirutka.cz>
+ */
+abstract class AbstractControllerIT extends Specification {
+
+    protected static final CONTENT_TYPE_JSON = "application/json;charset=UTF-8"
+
+    @Shared MockMvc mockMvc
+    @Shared controller
+    ResponseWrapper response
+
+    def GET = MockMvcRequestBuilders.&get
+    def POST = MockMvcRequestBuilders.&post
+    def PUT = MockMvcRequestBuilders.&put
+    def DELETE = MockMvcRequestBuilders.&delete
+
+
+    abstract def initController()
+    void setupController(_) {}
+
+
+    void setupSpec() {
+        controller = initController()
+        mockMvc = new AdvicedStandaloneMockMvcBuilder(controller)
+                .setControllerAdvices(new CommonExceptionHandler())
+                .build()
+    }
+
+    def setup() {
+        setupController(controller)
+    }
+
+
+    def perform(MockHttpServletRequestBuilder requestBuilder) {
+        def result = mockMvc.perform(requestBuilder).andReturn()
+        response = new ResponseWrapper(result.getResponse());
+    }
+
+
+    static class ResponseWrapper {
+
+        @Delegate
+        private final MockHttpServletResponse response
+
+        @Lazy json = new JsonSlurper().parseText(contentAsString)
+
+        ResponseWrapper(MockHttpServletResponse response) {
+            this.response = response
+        }
+    }
+}
---
+++ src/test/groovy/cz/cvut/zuul/oaas/api/resources/ClientsControllerIT.groovy
@@ -0,0 +1,110 @@
+package cz.cvut.zuul.oaas.api.resources
+
+import cz.cvut.zuul.oaas.Factories
+import cz.cvut.zuul.oaas.api.models.ClientDTO
+import cz.cvut.zuul.oaas.services.ClientsService
+import org.hibernate.validator.method.MethodConstraintViolationException
+import org.springframework.security.oauth2.provider.NoSuchClientException
+
+import static java.util.Collections.emptySet
+import static org.springframework.http.MediaType.APPLICATION_JSON
+
+/**
+ * @author Jakub Jirutka <jakub@jirutka.cz>
+ */
+class ClientsControllerIT extends AbstractControllerIT {
+
+    def service = Mock(ClientsService)
+
+    def initController() { new ClientsController() }
+    void setupController(_) { _.clientsService = service }
+
+
+    void 'GET: non existing client'() {
+        setup:
+            1 * service.findClientById('666') >> { throw new NoSuchClientException("") }
+        when:
+            perform GET('/v1/clients/666').with {
+                accept APPLICATION_JSON
+            }
+        then:
+            response.status == 404
+    }
+
+    void 'GET: existing client'() {
+        setup:
+            1 * service.findClientById(expected.clientId) >> expected
+        when:
+            perform GET('/v1/clients/42').with {
+                accept APPLICATION_JSON
+            }
+        then:
+            with(response) {
+                status == 200
+                contentType == CONTENT_TYPE_JSON
+
+                json.client_id              == expected.clientId
+                json.client_secret          == expected.clientSecret
+                json.resource_ids           == expected.resourceIds
+                json.authorized_grant_types == expected.authorizedGrantTypes
+                json.redirect_uri           == expected.registeredRedirectUri
+                json.access_token_validity  == expected.accessTokenValiditySeconds
+                json.refresh_token_validity == expected.refreshTokenValiditySeconds
+            }
+        where:
+            expected = Factories.createRandomClientDTO('42')
+    }
+
+    def 'POST: invalid client'() {
+        setup:
+            1 * service.createClient(_) >> { throw new MethodConstraintViolationException(emptySet()) }
+        when:
+            perform POST ('/v1/clients').with {
+                content '{ "scope": "something" }'
+                contentType APPLICATION_JSON
+            }
+        then:
+            response.status == 400
+    }
+
+    def 'POST: valid client'() {
+        setup:
+            ClientDTO client
+            service.createClient({ client = it }) >> '123'
+        when:
+            perform POST ('/v1/clients').with {
+                contentType APPLICATION_JSON
+                content """{
+                    "authorities": [ "ROLE_CLIENT" ],
+                    "authorized_grant_types": [ "refresh_token", "authorization_code" ],
+                    "redirect_uri": [ "http://example.org" ],
+                    "scope": [ "urn:ctu:oauth:dummy", "urn:ctu:oauth:oaas:check-token" ]
+                }"""
+            }
+        then:
+            response.status              == 201
+            response.redirectedUrl       == "/v1/clients/123"
+
+            client.authorities           == [ 'ROLE_CLIENT' ] as Set
+            client.authorizedGrantTypes  == [ "refresh_token", "authorization_code" ] as Set
+            client.registeredRedirectUri == [ "http://example.org" ] as Set
+            client.scope                 == [ "urn:ctu:oauth:dummy", "urn:ctu:oauth:oaas:check-token" ] as Set
+    }
+
+    def 'DELETE: non existing client'() {
+        setup:
+            1 * service.removeClient('666') >> { throw new NoSuchClientException("") }
+        when:
+            perform DELETE('/v1/clients/666')
+        then:
+            response.status == 404
+    }
+
+    def 'DELETE: existing client'() {
+        when:
+            perform DELETE('/v1/clients/123')
+        then:
+            response.status == 204
+            1 * service.removeClient('123')
+    }
+}
