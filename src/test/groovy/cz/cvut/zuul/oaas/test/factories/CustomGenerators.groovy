cd0880528eab08f4e57919f9ab9daa9bec6184ec
Jakub Jirutka <jakub@jirutka.cz>
2013-08-25 15:34:06 +0200

    Fix compile issue by removing @Samples from CustomGenerators
    This annotation triggers annotation processor of QuickCheck that
    generates Samples class for the annotated generator. However, the
    CustomGeneratorSamples class already exists and compiler didn't
    like that.

--- src/test/groovy/cz/cvut/zuul/oaas/test/factories/CustomGenerators.groovy
+++ src/test/groovy/cz/cvut/zuul/oaas/test/factories/CustomGenerators.groovy
@@ -1,14 +1,12 @@
 package cz.cvut.zuul.oaas.test.factories
 
 import net.java.quickcheck.Generator
-import net.java.quickcheck.srcgenerator.Samples
 
 import static net.java.quickcheck.generator.PrimitiveGeneratorSamples.anyLetterString
 
 /**
  * @author Jakub Jirutka <jakub@jirutka.cz>
  */
-@Samples
 class CustomGenerators {
 
     static Generator<String> emails() {
