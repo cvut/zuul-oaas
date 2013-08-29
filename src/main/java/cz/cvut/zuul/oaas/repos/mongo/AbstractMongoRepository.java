272fa2bc7cc2148fe9624cc2ff60fc76cc8e3f8d
Jakub Jirutka <jakub@jirutka.cz>
2013-08-29 20:32:57 +0200

    Move method from MongoUtils to AbstractMongoRepository

--- src/main/java/cz/cvut/zuul/oaas/repos/mongo/AbstractMongoRepository.java
+++ src/main/java/cz/cvut/zuul/oaas/repos/mongo/AbstractMongoRepository.java
@@ -1,10 +1,14 @@
 package cz.cvut.zuul.oaas.repos.mongo;
 
-import cz.cvut.zuul.oaas.utils.MongoUtils;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mapping.model.MappingException;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
+import org.springframework.data.mongodb.repository.support.MappingMongoEntityInformation;
 import org.springframework.data.mongodb.repository.support.SimpleMongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
@@ -33,7 +37,7 @@ public abstract class AbstractMongoRepository<T, ID extends Serializable> implem
     }
 
     @PostConstruct void initialize() {
-        MongoEntityInformation<T, ID> entityInformation = MongoUtils.createEntityInformation(entityClass, mongoOperations);
+        MongoEntityInformation<T, ID> entityInformation = createEntityInformation(entityClass, mongoOperations);
         mongoRepository = new SimpleMongoRepository<>(entityInformation, mongoOperations);
     }
 
@@ -93,6 +97,32 @@ public abstract class AbstractMongoRepository<T, ID extends Serializable> implem
 
     protected MongoOperations mongo() {
         return mongoOperations;
+    }
+
+    /**
+     * Returns the {@link MappingMongoEntityInformation} for the given entity class.
+     *
+     * <p>Implementation copied from {@link org.springframework.data.mongodb.repository.support.MongoRepositoryFactory#getEntityInformation(Class)}</p>
+     *
+     * @param <T> the entity type
+     * @param <ID> the id type
+     * @param entityClass
+     * @param mongoOperations
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    protected <T, ID extends Serializable>
+            MongoEntityInformation<T, ID> createEntityInformation(Class<T> entityClass, MongoOperations mongoOperations) {
+
+        MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext
+                = mongoOperations.getConverter().getMappingContext();
+        MongoPersistentEntity<?> entity = mappingContext.getPersistentEntity(entityClass);
+
+        if (entity == null) {
+            throw new MappingException(String.format(
+                    "Could not lookup mapping metadata for domain class %s!", entityClass.getName()));
+        }
+        return new MappingMongoEntityInformation<>((MongoPersistentEntity<T>) entity);
     }
 
 
