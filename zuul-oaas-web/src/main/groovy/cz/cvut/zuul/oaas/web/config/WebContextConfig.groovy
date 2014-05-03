46d05c8846ad91475114821f135cb4be37a64c71
Jakub Jirutka <jakub@jirutka.cz>
2014-05-04 01:33:35 +0200

    Replace thymeleaf-spring3 with thymeleaf-spring4

--- zuul-oaas-web/src/main/groovy/cz/cvut/zuul/oaas/web/config/WebContextConfig.groovy
+++ zuul-oaas-web/src/main/groovy/cz/cvut/zuul/oaas/web/config/WebContextConfig.groovy
@@ -34,8 +34,8 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import org.thymeleaf.spring3.SpringTemplateEngine
-import org.thymeleaf.spring3.view.ThymeleafViewResolver
+import org.thymeleaf.spring4.SpringTemplateEngine
+import org.thymeleaf.spring4.view.ThymeleafViewResolver
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver
 
 import javax.inject.Inject
